18923d5edf80fe8adac5845a97172e1860af1d36
Alexander Dietrich <AlexanderDietrich@gmx.at>
2017-09-26 13:41:51 +0200

    Test-Commit

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    printf("Hallo ich bin's, der Peter... und dein PC hängt sich jetzt auf\n");
+
+    for (int i = 0; i < 1000000000000; i ++){
+        std::cout << "HAHAHAHA !" << std::endl;
+    }
     return 0;
 }
