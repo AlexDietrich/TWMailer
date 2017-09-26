1bb51d5acaee16d2b2059653bdaed70b0ea5548b
Alexander dietrich <adietrich@localhost.localdomain>
2017-09-26 13:06:51 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/TWMailer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TWMailer.dir/main.cpp.o"
+  "TWMailer.pdb"
+  "TWMailer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TWMailer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
